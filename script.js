e52b820a493216abc39d21181e47c5d57ae55d4f
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-12-11 18:11:12 +0200

    Implement logic for getSize, getHead, getTail and atIndex methods

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { Node } from './node-class';
+import { Node } from './node-class.js';
 
 class LinkedList {
   constructor() {
@@ -23,5 +23,39 @@ class LinkedList {
     const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
+  }
+
+  getSize() {
+    let size = 0;
+    let current = this.head;
+    while (current !== null) {
+      size++;
+      current = current.next;
+    }
+    return size;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    let current = this.head;
+    while (current !== null) {
+      if (current.next === null) {
+        return current;
+      }
+      current = current.next;
+    }
+  }
+
+  atIndex(index) {
+    let current = this.head;
+    let count = 1;
+    while (count <= index) {
+      count++;
+      current = current.next;
+    }
+    return current;
   }
 }
