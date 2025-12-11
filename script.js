c6fbda56bc4498ef3ef5f3a80bdae6f283869dcf
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-12-11 17:53:39 +0200

    Implement append and prepend methods

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
 }
