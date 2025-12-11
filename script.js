4dae843114b8840bf53d72efe1bc01bb18cbb3ed
Durotaren <188862641+Durotaren@users.noreply.github.com>
2025-12-11 21:50:09 +0200

    Add pop logic and also add safeguards for more methods

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ class LinkedList {
   }
 
   getSize() {
+    if (this.head === null) {
+      return 'Linked List is empty.';
+    }
+
     let size = 0;
     let current = this.head;
     while (current !== null) {
@@ -36,10 +40,18 @@ class LinkedList {
   }
 
   getHead() {
+    if (this.head === null) {
+      return 'Linked List is empty.';
+    }
+
     return this.head;
   }
 
   getTail() {
+    if (this.head === null) {
+      return 'Linked List is empty.';
+    }
+
     let current = this.head;
     while (current !== null) {
       if (current.next === null) {
@@ -50,12 +62,47 @@ class LinkedList {
   }
 
   atIndex(index) {
+    if (this.head === null) {
+      return 'Linked List is empty.';
+    }
+
     let current = this.head;
     let count = 1;
+
     while (count <= index) {
       count++;
       current = current.next;
     }
+
     return current;
   }
+
+  pop() {
+    if (this.head === null) {
+      return;
+    }
+
+    if (this.head.next === null) {
+      this.head = null;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next.next !== null) {
+      current = current.next;
+    }
+    current.next = null;
+  }
 }
+
+const list = new LinkedList();
+
+list.append('dog');
+list.append('cat');
+list.append('parrot');
+list.append('hamster');
+list.append('snake');
+list.append('turtle');
+
+list.pop();
+console.log(list.getTail());
